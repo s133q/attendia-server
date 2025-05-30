722284a6c7aae8b99b9e7474bc8afb79b80028b2
s133q <olyaorop@gmail.com>
2025-05-30 20:35:21 +0300

    Delete attendia-server directory

--- attendia-server/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///attendia.db")
-SQLALCHEMY_TRACK_MODIFICATIONS = False

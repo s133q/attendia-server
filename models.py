8603a11380d4dfd12f951ad2f358f94d434d1464
s133q <olyaorop@gmail.com>
2025-06-01 00:03:34 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,6 +5,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 class SyncItem(db.Model):

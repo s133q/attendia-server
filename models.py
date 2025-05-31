60c71bfc3eecccc857e8d0a124d0faee26ceb6b6
s133q <olyaorop@gmail.com>
2025-05-31 14:34:41 +0300

    Update models.py

--- models.py
+++ models.py
@@ -7,4 +7,10 @@ class User(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+class SyncItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    content = db.Column(db.String(500))
+    last_modified = db.Column(db.DateTime, default=datetime.utcnow)
+
 # Можеш пізніше додати тут Group, Lesson, Attendance тощо

45b9608e60c53ea36b8a67bf151bc7c8202bfa2f
s133q <olyaorop@gmail.com>
2025-06-02 20:12:21 +0300

    Update groups.py

--- routes/groups.py
+++ routes/groups.py
@@ -1,1 +1,10 @@
+from flask import Blueprint, jsonify
+from models import Group
+
+groups_bp = Blueprint('groups', __name__)
+
+@groups_bp.route('/', methods=['GET'])
+def get_groups():
+    groups = Group.query.all()
+    return jsonify([{'id': g.id, 'name': g.name} for g in groups])
 

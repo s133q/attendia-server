02c7e8a7b7e57169af6bcde9554298e9fa1718f4
s133q <olyaorop@gmail.com>
2025-06-02 18:57:12 +0300

    Update models.py

--- models.py
+++ models.py
@@ -8,12 +8,12 @@ class User(db.Model):
     first_name = db.Column(db.String(120), nullable=False)
     last_name = db.Column(db.String(120), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    token = db.Column(db.String(36), unique=True)
+
 
 class SyncItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     content = db.Column(db.String(500))
     last_modified = db.Column(db.DateTime, default=datetime.utcnow)
-    token = db.Column(db.String(36), unique=True)
-
 # Можеш пізніше додати тут Group, Lesson, Attendance тощо

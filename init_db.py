0c75fe44237e2bd2a797a95c072be7fa9818f2e4
s133q <olyaorop@gmail.com>
2025-06-02 20:14:33 +0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,6 +1,8 @@
-from app import app
+from app import create_app
 from database import db
+
+app = create_app()
 
 with app.app_context():
     db.create_all()
-    print("Database initialized.")
+    print("Database tables created.")

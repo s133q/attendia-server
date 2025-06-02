dfc889748e232ea0783cf1b61a9ebb9dc182b2ba
s133q <olyaorop@gmail.com>
2025-06-02 20:13:27 +0300

    Update lessons.py

--- routes/lessons.py
+++ routes/lessons.py
@@ -1,1 +1,10 @@
+from flask import Blueprint, jsonify
+from models import Lesson
+
+lessons_bp = Blueprint('lessons', __name__)
+
+@lessons_bp.route('/', methods=['GET'])
+def get_lessons():
+    lessons = Lesson.query.all()
+    return jsonify([{'id': l.id, 'title': l.title} for l in lessons])
 

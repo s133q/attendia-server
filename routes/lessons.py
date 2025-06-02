97348d5e4c0df2efd349b8068d6bb325fd3501fb
s133q <olyaorop@gmail.com>
2025-06-03 02:42:00 +0300

    Update lessons.py

--- routes/lessons.py
+++ routes/lessons.py
@@ -1,10 +1,24 @@
-from flask import Blueprint, jsonify
-from models import Lesson
+from flask import Blueprint, request, jsonify
+from models import Lesson, Group
+from database import db
 
 lessons_bp = Blueprint('lessons', __name__)
 
-@lessons_bp.route('/', methods=['GET'])
-def get_lessons():
-    lessons = Lesson.query.all()
-    return jsonify([{'id': l.id, 'title': l.title} for l in lessons])
+@lessons_bp.route('/', methods=['POST'])
+def create_lesson():
+    data = request.get_json()
+    if not data or not all(k in data for k in ('title', 'group_id')):
+        return jsonify({'error': 'Missing required fields'}), 400
 
+    if not Group.query.get(data['group_id']):
+        return jsonify({'error': 'Group not found'}), 404
+
+    new_lesson = Lesson(title=data['title'], group_id=data['group_id'])
+    db.session.add(new_lesson)
+    db.session.commit()
+    return jsonify({'message': 'Lesson created successfully'}), 201
+
+@lessons_bp.route('/group/<int:group_id>', methods=['GET'])
+def get_lessons(group_id):
+    lessons = Lesson.query.filter_by(group_id=group_id).all()
+    return jsonify([{'id': l.id, 'title': l.title} for l in lessons]), 200

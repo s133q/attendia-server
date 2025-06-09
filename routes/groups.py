8c905d6a54a678c5c95ab6dd60c24c1734b378c3
s133q <olyaorop@gmail.com>
2025-06-09 13:33:20 +0300

    Update groups.py

--- routes/groups.py
+++ routes/groups.py
@@ -23,14 +23,14 @@ def get_groups(user_id):
     groups = Group.query.filter_by(user_id=user_id).all()
     return jsonify([{'id': g.id, 'name': g.name} for g in groups]), 200
 
-@groups_bp.route('/group/<int:group_id>', methods=['GET'])
+@groups_bp.route('/<int:group_id>', methods=['GET'])
 def get_group_by_id(group_id):
     group = Group.query.get(group_id)
     if not group:
         return jsonify({'error': 'Group not found'}), 404
     return jsonify({'id': group.id, 'name': group.name, 'user_id': group.user_id}), 200
 
-@groups_bp.route('/group/full/<int:group_id>', methods=['GET'])
+@groups_bp.route('/full/<int:group_id>', methods=['GET'])
 def get_full_group_data(group_id):
     group = Group.query.get(group_id)
     if not group:
